8b8e42c2518cc990595b75b42f001ff795831d5b
JceLee <lkm4351@gmail.com>
2021-01-29 12:48:43 +0900

    setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
 import React from "react";
-import {BrowserRouter as Router, Route} from "react-router-dom";
-import MainView from "./view/MainView"
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import MainView from "./view/MainView";
 import Arrangements from "./view/Arrangements";
+import "./App.scss";
 
 export default function App() {
   return (
     <>
-    <h3 style={{width : "70%", margin : "0 auto"}}>The Flower Room</h3>
-    <Router>
-      <Route exact path="/" component={MainView}/>
-      <Route exact path="/arrangements" component={Arrangements}/>
-    </Router>
-      </>
+      <h3 style={{ width: "70%", margin: "0 auto" }}>The Flower Room</h3>
+      <Router>
+        <Route exact path="/" component={MainView} />
+        <Route exact path="/arrangements" component={Arrangements} />
+      </Router>
+    </>
   );
 }
